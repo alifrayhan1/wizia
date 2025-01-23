390fdba5e5dafbd3f3705d52392d4894cf80fca6
alifrayhan <alifrayhan92@gmail.com>
2025-01-23 22:27:28 +0600

    bannar section done

--- Wizia/src/Component/Bannar.jsx
+++ Wizia/src/Component/Bannar.jsx
@@ -5,17 +5,17 @@ function Bannar() {
   return (
     <>
       <section className="bg-[url('Img.svg')] bg-cover bg-no-repeat">
-        <div className="space-y-5 p-10 sm:p-16">
-          <h3 className="text-lg text-secondary text-center sm:text-start">AI SDRs (aiDRs)</h3>
+        <div className="space-y-6 p-10 sm:p-16">
+          <h3 className="text-lg sm:text-xl text-secondary text-center sm:text-start">AI SDRs (aiDRs)</h3>
           <h1 className="text-2xl sm:text-3xl text-white text-center sm:text-start sm:w-3/12">
             More <span className="italic">leads</span>, less
             <span className="italic"> people</span>
           </h1>
-          <p className="text-xs sm:text-sm text-whiteShades sm:w-1/2 text-center sm:text-start">
+          <p className="text-sm sm:text-lg text-whiteShades sm:w-1/2 text-center sm:text-start">
             Train an aiDR on your ICP and messaging matrix. Activate it on a
             patch. It will send personalized sequences to every target contact.
           </p>
-          <div className="flex justify-center items-center">
+          <div className="flex justify-center items-center sm:justify-start">
           <Button
             color="primary"
             className="bg-primary rounded-full"
