777101f9bb087b405d99559d1c7498ef288d51d5
alifrayhan <alifrayhan92@gmail.com>
2025-01-23 22:54:45 +0600

    errort section complete

--- Wizia/src/Component/Effort.jsx
+++ Wizia/src/Component/Effort.jsx
@@ -20,9 +20,9 @@ function Effort() {
       <section className="bg-[url('effort.svg')] bg-cover bg-no-repeat bg-center p-10 sm:p-16">
         <div className="space-y-10">
           <div className="space-y-2">
-            <h1 className="text-white text-xl sm:text-2xl w-3/5">Allocate effort where your reps make an inpact.</h1>
-            <h2 className="text-primary text-lg sm:text-xl italic">Let us handle the rest.</h2>
-            <p className="text-whiteShades text-xs sm:text-sm w-3/5">Keep your reps “in the air” -- out in the field and on the phone where they can build relationships.</p>
+            <h1 className="text-white text-2xl sm:text-3xl font-semibold sm:font-bold w-3/5">Allocate effort where your reps make an inpact.</h1>
+            <h2 className="text-primary text-xl sm:text-2xl font-semibold italic">Let us handle the rest.</h2>
+            <p className="text-whiteShades text-sm sm:text-base w-3/5">Keep your reps “in the air” -- out in the field and on the phone where they can build relationships.</p>
             </div>
             <div className="flex flex-col sm:flex-row space-y-2">
               
